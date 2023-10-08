bff4df9f8eeb0c35dc96b7b29a84edb572fba940
Darius Brown <dariustb@tech-center.com>
2023-10-08 11:27:19 -0400

    Setup site layout

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import viteLogo from '/vite.svg'
+// import nt_hero from '/nt_hero.png'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+document.querySelector('#hero').innerHTML = `
+  <section>
+    <h1>Hello Hero</h1>
+  </section>
 `
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#bio').innerHTML = `
+  <section>
+    <h1>Hello Bio</h1>
+  </section>
+`
+
+document.querySelector('#discog').innerHTML = `
+  <section>
+    <h1>Hello Discog</h1>
+  </section>
+`
+
+document.querySelector('#footer').innerHTML = `
+  <section>
+    <h1>Hello Footer</h1>
+  </section>
+`
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
